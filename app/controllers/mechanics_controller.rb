bfcdfa9983d4419313e0051e3631a55c8d2af375
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-09-12 10:08:36 -0600

    tests for us2

--- app/controllers/mechanics_controller.rb
+++ app/controllers/mechanics_controller.rb
@@ -3,5 +3,9 @@ class MechanicsController < ApplicationController
   def index
     @mechanics = Mechanic.all
   end
-  
+
+  def show
+    @mechanic = Mechanic.find(params[:id])
+  end
+
 end
--- spec/features/mechanics/show_spec.rb
+++ spec/features/mechanics/show_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe 'mechanics index page', type: :feature do
+RSpec.describe 'mechanic show page', type: :feature do
   describe 'As a visitor' do
-    describe 'When I visit the mechanics index page' do
+    describe 'When I visit the mechanics show page' do
         before :each do
           @six_flags = AmusementPark.create!(name: 'Six Flags', admission_cost: 75)
           @universal = AmusementPark.create!(name: 'Universal Studios', admission_cost: 80)
@@ -13,12 +13,12 @@ RSpec.describe 'mechanics index page', type: :feature do
           @scrambler = @six_flags.rides.create!(name: 'The Scrambler', thrill_rating: 4, open: true)
           @ferris = @six_flags.rides.create!(name: 'Ferris Wheel', thrill_rating: 7, open: false)
           
-          @jaws = @universal.rides.create!(name: 'Jaws', thrill_rating: 5, open: true)
           @teacups = @universal.rides.create!(name: 'Teacups', thrill_rating: 2, open: true)
+          @jaws = @universal.rides.create!(name: 'Jaws', thrill_rating: 5, open: true)
           
           @superman = @disney.rides.create!(name: 'Superman', thrill_rating: 9, open: false)
           @little_mermaid = @disney.rides.create!(name: 'Little Mermaid', thrill_rating: 3, open: true)
-          @smallworld = @disney.rides.create!(name: 'Small World', thrill_rating: 5, open: true)
+          @smallworld = @disney.rides.create!(name: 'Small World', thrill_rating: 4, open: true)
           
           @alaina = Mechanic.create!(name:"Alaina", years_experience: 6)
           @ryan = Mechanic.create!(name:"Ryan", years_experience: 7)
@@ -27,39 +27,32 @@ RSpec.describe 'mechanics index page', type: :feature do
           @sydney = Mechanic.create!(name:"Sydney", years_experience: 9)
           
           @alaina_superman = MechanicRide.create!(mechanic_id: @alaina.id, ride_id: @superman.id)
-          @ryan_superman = MechanicRide.create!(mechanic_id: @ryan.id, ride_id: @superman.id)
           @alaina_jaws = MechanicRide.create!(mechanic_id: @alaina.id, ride_id: @jaws.id)
+          @alaina_teacups = MechanicRide.create!(mechanic_id: @alaina.id, ride_id: @teacups.id)
+          @alaina_smallworld = MechanicRide.create!(mechanic_id: @alaina.id, ride_id: @smallworld.id)
+          @ryan_superman = MechanicRide.create!(mechanic_id: @ryan.id, ride_id: @superman.id)
           @whitney_jaws = MechanicRide.create!(mechanic_id: @whitney.id, ride_id: @jaws.id)
           @eddie_smallworld = MechanicRide.create!(mechanic_id: @eddie.id, ride_id: @smallworld.id)
           @sydney_teacups = MechanicRide.create!(mechanic_id: @sydney.id, ride_id: @teacups.id)
-          @alaina_teacups = MechanicRide.create!(mechanic_id: @alaina.id, ride_id: @teacups.id)
         end
 
-      it 'I see a header saying All Mechanics' do
+      it 'I see their name, years of experience, and the names of rides theyre working on
+      And I only see rides that are open' do
 
-        visit "/mechanics"
-        expect(page).to have_content('All Mechanics')
+        visit "/mechanics/#{@alaina.id}"
+        expect(page).to have_content("Mechanic Name:#{@alaina.name}")
+        expect(page).to have_content("Years Experience:#{@alaina.years_experience}")
+        expect(page).to_not have_content("#{@ryan.name}")
+        expect(page).to have_content("Teacups")
+        expect(page).to have_content("Jaws")
+        expect(page).to have_content("Small World'")
+        expect(page).to_not have_content("Superman")
       end
 
-      it 'And I see a list of all mechanics names and their years of experience' do
-        visit "/mechanics"
-        expect(page).to have_content('Alaina')
-        expect(page).to have_content('Ryan')
-        expect(page).to have_content('Eddie')
-        expect(page).to have_content('Whitney')
-        expect(page).to have_content('Sydney')
-        expect(page).to_not have_content('Sally')
-        within("#mechanic_#{@alaina.id}") do 
-          expect(page).to have_content("Years Experience:#{@alaina.years_experience}")
-          expect(page).to_not have_content("Years Experience:#{@eddie.years_experience}")
-        end
-
-      end
-
-      it 'And I see the average years of experience across all mechanics' do
-        visit "/mechanics"
-          expect(page).to have_content('Average Years Experience of All Mechanics:7')
-          expect(page).to_not have_content('Average Years Experience of All Mechanics: 40')
+      xit 'And the rides are listed by thrill rating in descending order (most thrills first)' do
+        visit "/mechanics/#{@alaina.id}"
+        expect("Jaws").to appear_before('Small World')
+        expect("Small World").to appear_before('Teacup')
       end
 
     end
